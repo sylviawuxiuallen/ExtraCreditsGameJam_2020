534fb475fed620299e73e78f67eb4115fced202e
Hellspoodles <hellspoodles@gmail.com>
2020-08-23 02:41:34 -0700

    Added art assets

--- Assets/Scripts/Resources/Resource_Tree.cs
+++ Assets/Scripts/Resources/Resource_Tree.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Resource_Tree : NaturalSite
 {
-
+    public List<Sprite> treeSprites;
     void Start()
     {
         siteName = NaturalSiteType.TYPE_TREE;
         //vary size and position a little
         Vector3 pos = (Random.insideUnitSphere * 0.25f);
-        pos.z = 0;
+        pos.z = transform.position.y / 200;
         transform.position += pos;
         transform.localScale = transform.localScale * (Random.value + 1);
+        gameObject.GetComponent<SpriteRenderer>().sprite = treeSprites[Mathf.FloorToInt(Random.value * treeSprites.Count)];
     }
 }
